4f5628cd5a87e0f5f70cb5d480c20d9050ee40e8
Salil Wadnerkar <rohshall@gmail.com>
2012-12-09 17:36:14 +0800

    Working version

--- hnews.hs
+++ hnews.hs
@@ -1,14 +1,54 @@
-import Data.Aeson
-import Data.Attoparsec
-import Data.ByteString
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Aeson (FromJSON, decode)
+import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.ByteString.Char8 as BS
 import Data.Maybe
 import Network.HTTP
 import Network.URI
 
+import GHC.Generics (Generic)
+
+data Article = Article { title :: BS.ByteString,
+                         url :: BS.ByteString,
+                         id :: Integer,
+                         commentCount :: Integer,
+                         points :: Integer,
+                         postedAgo :: BS.ByteString,
+                         postedBy :: BS.ByteString
+               } deriving (Show, Generic)
+
+data Articles = Articles {nextId :: Maybe Integer, 
+                          items  :: [Article],
+                          version :: BS.ByteString,
+                          cachedOnUTC :: BS.ByteString}
+                deriving (Show, Generic)
+
+instance FromJSON Articles
+instance FromJSON Article
 
 main = do
-  feed <- openURL "http://hndroidapi.appspot.com/news/format/json/page/"
-  print $ parse json feed
+  feed <- openURL "http://api.ihackernews.com/page"
+  let lazyFeed = BL.fromStrict feed
+  let articles = decode lazyFeed :: Maybe Articles
+  case articles of
+    Just as -> printArticles as
+    Nothing -> error "No Articles"
 
-openURL :: String -> IO ByteString
+
+printArticles :: Articles -> IO()
+printArticles articles = mapM_ printArticle $ items articles
+
+
+printArticle :: Article -> IO()
+printArticle article = do
+  putStrLn $ (show $ Main.id article) ++ ": " ++ (show $ title article)
+  putStrLn $ show $ url article
+  putStrLn $ "(" ++ (show $ commentCount article) ++ " comments) (" ++ (show $ points article) ++ " points)"
+  putStrLn ""
+
+
+openURL :: String -> IO BS.ByteString
 openURL url = getResponseBody =<< simpleHTTP (mkRequest GET (fromJust $ parseURI url))
+
