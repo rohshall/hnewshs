4ece0dba2089698b8b711c3c4a33a12cfbbba249
Salil Wadnerkar <salil@adid.sg>
2013-05-07 17:29:02 +0800

    fixed xml parsing

--- hnews.hs
+++ hnews.hs
@@ -4,13 +4,19 @@ import Data.Maybe
 import Network.HTTP.Wget
 import Text.XML.Light
 
+
+showArticle :: Element -> Maybe String
+showArticle elem = do
+    titleElem <- findChild (unqual "title") elem
+    linkElem <- findChild (unqual "link") elem
+    return $ concat $ map (\c -> strContent c ++ "\n") [titleElem, linkElem]
+
+
 main = do
-  feed <- wget "https://news.ycombinator.com/rss" [] []
-  case parseXMLDoc feed of
-    Nothing -> error "Failed to parse the feed"
-    Just doc -> 
-      let _:_:elem:_ = findElements (unqual "description") $ doc
-          [Text title_content] = elContent elem
-          title_text = cdData title_content
-      in mapM_ putStrLn $ lines title_text
+    feed <- wget "https://news.ycombinator.com/rss" [] []
+    case parseXMLDoc feed of
+        Nothing -> error "Failed to parse the feed"
+        Just doc -> 
+            let articles = findElements (unqual "item") $ doc
+            in mapM_ (putStrLn.fromJust.showArticle) articles
 
