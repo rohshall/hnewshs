e30609cccf5b4d1244565775caaac0991fa66a55
Salil Wadnerkar <rohshall@gmail.com>
2013-05-07 09:25:20 +0800

    Added XML parsing of the RSS feed

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 hnewshs
 =======
 
-Hacker News Reader in Haskell
+Hacker News Reader in Haskell, implemented by parsing the RSS feed
 
-You will need the install following packages using cabal:  
 
-* aeson (Data.Aeson) (https://github.com/bos/aeson). I am using the latest package from github, which supports json parsing error reporting through eitherDecode.
-* network
-* http
+### Installation
+
+```cabal install http-wget```
+
+```cabal install xml```
+
+```runhaskell hnews.hs```
--- hnews.hs
+++ hnews.hs
@@ -1,37 +1,16 @@
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE OverloadedStrings #-}
-
-import Data.Aeson (FromJSON, eitherDecode)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Lazy as BL
 import Data.Maybe
-import Network.HTTP
-import Network.URI
-
-import GHC.Generics (Generic)
-
-data Article = Article { rank :: BS.ByteString,
-                         title :: BS.ByteString,
-                         url :: BS.ByteString
-               } deriving (Show, Generic)
-
-instance FromJSON Article
+import Network.HTTP.Wget
+import Text.XML.Light
 
 main = do
-  feed <- openURL "http://apify.heroku.com/api/hacker_news.json"
-  let parseResult = eitherDecode feed :: Either String [Article]
-  case parseResult of
-    Right articles -> mapM_ printArticle articles
-    Left s -> error ("Server error: " ++ s)
-
-
-printArticle :: Article -> IO()
-printArticle article = do
-  putStrLn $ (show $ rank article) ++ ": " ++ (show $ title article)
-  putStrLn $ show $ url article
-  putStrLn ""
-
-
-openURL :: String -> IO BL.ByteString
-openURL url = getResponseBody =<< simpleHTTP (mkRequest GET (fromJust $ parseURI url))
+  feed <- wget "https://news.ycombinator.com/rss" [] []
+  case parseXMLDoc feed of
+    Nothing -> error "Failed to parse the feed"
+    Just doc -> 
+      let _:_:elem:_ = findElements (unqual "description") $ doc
+          [Text title_content] = elContent elem
+          title_text = cdData title_content
+      in mapM_ putStrLn $ lines title_text
 
